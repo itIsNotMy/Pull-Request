0e34972e73bd3007da78ec0bd7108974cdc1ce09
Misha <mixail_y1@mail.ru>
2021-10-14 20:30:08 +0000

    auth corrected

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -4,7 +4,7 @@
     <h2 class="blog-post-title">{{ $article->title }}</h2>
     <p class="blog-post-meta">{{ $article->text }}</p>
     <p class="blog-post-meta">{{ $article->datePublished }}</p>
-    @if(!empty(Auth::user()->id) && Auth::user()->id == $article->owner_id)
+    @can('update', $article)
         <a href="{{ route('articles.edit', $article) }}">Редактировать</a></h2>
-    @endif
+    @endcan
 @endsection
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -38,7 +38,9 @@
       <a class="p-2 text-muted" href="{{ route('articles.index') }}">Главная</a>
       <a class="p-2 text-muted" href="{{ route('about') }}">О нас</a>
       <a class="p-2 text-muted" href="{{ route('contacts') }}">Контакты</a>
+      @can('create', $article ?? '')
       <a class="p-2 text-muted" href="{{ route('articles.create') }}">Создать статью</a>
+      @endcan
       <a class="p-2 text-muted" href="{{ route('admin.feedback') }}">Админ. раздел</a>
     </nav>
   </div>
